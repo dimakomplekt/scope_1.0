3953d0ad78891a2df04ce7ccf55d913559a5033c
dimakomplekt <dkriskovets@gmail.com>
2026-06-09 13:06:17 +0300

    scope job

--- lib/scope/scope.c
+++ lib/scope/scope.c
@@ -9,3 +9,109 @@
 #include "../sin_generator/sin_generator.h"
 
 // =========================================================================================== IMPORT
+
+
+
+
+// =========================================================================================== Helper-functions predeclare
+
+void scope_buffer_init(scope_ctx* used_scope);
+
+void scope_gui_init(scope_ctx* used_scope, SDL_Renderer* renderer);
+void scope_gui_renew(scope_ctx* used_scope);
+
+
+// =========================================================================================== Helper-functions predeclare
+
+
+// =========================================================================================== API
+
+void scope_init(scope_ctx* used_scope, SDL_Renderer* renderer)
+{
+    // Main data init
+
+    used_scope->main_settings.current_mode = SCOPE_MODE_SCROLL_TO_LEFT;     // Базово - скролл
+    used_scope->main_settings.periods_to_display = 2;                       // Базово - 2 периода для отображения (в режиме с фикс. кол-вом)
+    used_scope->main_settings.current_signal_units  = VOLTS;                // Базово - вольты 
+    used_scope->main_settings.current_time_units = MICROSECONDS;            // Базово - микросекунды
+
+    scope_buffer_init(used_scope);
+
+    // No signal at the start
+    used_scope->signal_control_data.controlled_signal = NULL;
+
+    // GUI init and instant renew to setup
+    scope_gui_init(used_scope, renderer);
+    scope_gui_renew(used_scope);
+}
+
+
+void signal_check(scope_ctx* used_scope, sin_generator_ctx* controlled_signal)
+{
+
+}
+
+
+void scope_update(scope_ctx* used_scope)
+{
+
+}
+
+
+void scope_render(scope_ctx* used_scope)
+{
+
+}
+
+
+void scope_buffer_init(scope_ctx* used_scope)
+{
+    /*
+
+    Очистить массивы времён и значений (не обязательно для работы кольцевого буфера, но удобно для отладки).
+
+    Установить head = 0 — первая запись пойдёт в начало массива.
+
+    Установить count = 0 — данных пока нет.
+
+    */
+
+    scope_buffer_ctx* buffer = &used_scope->signal_control_data.scope_buffer_data;
+
+    memset(buffer->timestamps, 0, sizeof(buffer->timestamps));
+    memset(buffer->samples_values, 0, sizeof(buffer->samples_values));
+
+    buffer->head = 0;
+    buffer->count = 0;
+}
+
+
+void scope_gui_init(scope_ctx* used_scope, SDL_Renderer* renderer)
+{
+    // Инициализация рендерера
+    used_scope->scope_render_data.renderer = renderer;
+
+
+    // Строим осциллограф по сетке, используя 50 пикселей на единицу сетки 
+    used_scope->scope_render_data.basic_pixels_quantity_in_equivalent_unit = 50;
+
+
+    // Базово - 1 вольт, 100 мкс на единицу сетки
+    used_scope->scope_render_data.current_signal_scale = 1;
+    used_scope->scope_render_data.current_time_scale = 100;
+
+}
+
+
+void scope_gui_renew(scope_ctx* used_scope)
+{
+
+}
+
+
+void scope_destroy(scope_ctx* used_scope)
+{
+
+}
+
+// =========================================================================================== API
--- lib/scope/scope.h
+++ lib/scope/scope.h
@@ -50,18 +50,280 @@ typedef enum scope_mode
 } scope_mode_en;
 
 
+typedef enum signal_units
+{
+
+    VOLTS
+
+} signal_units;
+
+
+
+typedef enum time_units
+{
+
+    NANOSECONDS,
+    MICROSECONDS,
+    MILLISECONDS,
+    SECONDS
+    
+} time_units;
+
+
 // ===== Scope render data =====
+
+// Структура данных основных фигур для рендера
+
+typedef struct scope_base_gui_figures
+{
+    /*
+
+        Данные для вызова функций рендера фигур (в данной версии - задник, дисплей, сетка)
+
+        void my_sdl_draw_line(
+            
+            SDL_Renderer* renderer,
+            int x1, int y1,
+            int x2, int y2,
+            SDL_Color color
+                            
+        );
+
+
+        void my_sdl_draw_filled_rect(
+
+            SDL_Renderer* renderer,
+            int x, int y,
+            int w, int h,
+            SDL_Color fill_color,
+            SDL_Color border_color,
+            int border_thickness
+
+        );
+
+    */
+
+    // ===== Задник =====
+
+    int background_x;
+    int background_y;
+
+    int background_w;
+    int background_h;
+
+    SDL_Color background_fill_color;
+
+    SDL_Color background_border_color;
+
+    int background_border_thicknes;
+
+
+    // ===== Дисплей =====
+
+    int display_x;
+    int display_y;
+
+    int display_w;
+    int display_h;
+
+    SDL_Color display_fill_color;
+
+    SDL_Color display_border_color;
+
+    int display_border_thicknes;
+
+    // ===== Сетка =====
+
+    // Line 1
+
+    int line_1_x1;
+    int line_1_y1;
+
+    int line_1_x2;
+    int line_1_y2;
+
+    SDL_Color line_1_color;
+
+
+    // Line 2
+
+    int line_2_x1;
+    int line_2_y1;
+    
+    int line_2_x2;
+    int line_2_y2;
+
+    SDL_Color line_2_color;
+
+
+    // Line 3
+
+    int line_3_x1;
+    int line_3_y1;
+    
+    int line_3_x2;
+    int line_3_y2;
+
+    SDL_Color line_3_color;
+
+
+    // Line 4
+
+    int line_4_x1;
+    int line_4_y1;
+    
+    int line_4_x2;
+    int line_4_y2;
+
+    SDL_Color line_4_color;
+
+
+    // Line 5
+
+    int line_5_x1;
+    int line_5_y1;
+    
+    int line_5_x2;
+    int line_5_y2;
+
+    SDL_Color line_5_color;
+
+
+    // Line 6
+
+    int line_6_x1;
+    int line_6_y1;
+    
+    int line_6_x2;
+    int line_6_y2;
+
+    SDL_Color line_6_color;
+
+
+    // Line 7
+
+    int line_7_x1;
+    int line_7_y1;
+    
+    int line_7_x2;
+    int line_7_y2;
+
+    SDL_Color line_7_color;
+
+
+    // Line 8
+
+    int line_8_x1;
+    int line_8_y1;
+    
+    int line_8_x2;
+    int line_8_y2;
+
+    SDL_Color line_8_color;
+
+
+    // Line 9
+
+    int line_9_x1;
+    int line_9_y1;
+    
+    int line_9_x2;
+    int line_9_y2;
+
+    SDL_Color line_9_color;
+
+
+    // Line 10
+
+    int line_10_x1;
+    int line_10_y1;
+    
+    int line_10_x2;
+    int line_10_y2;
+
+    SDL_Color line_10_color;
+
+
+    // Line 11
+
+    int line_11_x1;
+    int line_11_y1;
+    
+    int line_11_x2;
+    int line_11_y2;
+
+    SDL_Color line_11_color;
+
+
+    // Line 12
+
+    int line_12_x1;
+    int line_12_y1;
+    
+    int line_12_x2;
+    int line_12_y2;
+
+    SDL_Color line_12_color;
+
+
+    // Line 13
+
+    int line_13_x1;
+    int line_13_y1;
+    
+    int line_13_x2;
+    int line_13_y2;
+
+    SDL_Color line_13_color;
+
+
+    // Line 14
+
+    int line_14_x1;
+    int line_14_y1;
+    
+    int line_14_x2;
+    int line_14_y2;
+
+    SDL_Color line_14_color;
+
+
+    // Line 15
+
+    int line_15_x1;
+    int line_15_y1;
+    
+    int line_15_x2;
+    int line_15_y2;
+
+    SDL_Color line_15_color;
+
+
+    // Line 16
+
+    int line_16_x1;
+    int line_16_y1;
+    
+    int line_16_x2;
+    int line_16_y2;
+
+    SDL_Color line_16_color;
+
+
+} scope_base_gui_figures;
+
+
 
 typedef struct scope_render {
 
+    // Ссылка на рендерер
+    SDL_Renderer* renderer;
+
     // Глобальные настройки
-
-    scope_mode_en current_mode;                     // Режим
-
-    int periods_to_display;                         // Количество периодов для отображения (в режиме с фикс. кол-вом)
 
     int basic_pixels_quantity_in_equivalent_unit;
 
+    // Текущие масштабы для отображения (общее значение сигнала или времени на единицу сетки)
     int current_signal_scale;
     int current_time_scale;
 
@@ -84,63 +346,104 @@ typedef struct scope_render {
     // Флаг для апдейта объектов GUI при смене настроек
     bool scope_gui_need_update;
 
+
+    scope_base_gui_figures figures;                      // Данные для рендера фигур (в данной версии - задник, дисплей, сетка)
+
     // Объекты (текстовые блоки)
-    Textbox* scope_signature_textbox;
+    Textbox scope_signature_textbox;
 
-    Textbox* signal_value_0_textbox;
-    Textbox* signal_value_1_textbox;
-    Textbox* signal_value_2_textbox;
-    Textbox* signal_value_3_textbox;
-    Textbox* signal_value_4_textbox;
-    Textbox* signal_value_5_textbox;
-    Textbox* signal_value_6_textbox;
-    Textbox* signal_value_7_textbox;
+    Textbox signal_value_0_textbox;
+    Textbox signal_value_1_textbox;
+    Textbox signal_value_2_textbox;
+    Textbox signal_value_3_textbox;
+    Textbox signal_value_4_textbox;
+    Textbox signal_value_5_textbox;
+    Textbox signal_value_6_textbox;
+    Textbox signal_value_7_textbox;
 
-
-    Textbox* time_value_0_textbox;
-    Textbox* time_value_1_textbox;
-    Textbox* time_value_2_textbox;
-    Textbox* time_value_3_textbox;
-    Textbox* time_value_4_textbox;
-    Textbox* time_value_5_textbox;
-    Textbox* time_value_6_textbox;
-    Textbox* time_value_7_textbox;
+    Textbox signal_unit_textbox;
 
 
-    Textbox* change_value_scale_instruction_textbox;
+    Textbox time_value_0_textbox;
+    Textbox time_value_1_textbox;
+    Textbox time_value_2_textbox;
+    Textbox time_value_3_textbox;
+    Textbox time_value_4_textbox;
+    Textbox time_value_5_textbox;
+    Textbox time_value_6_textbox;
+    Textbox time_value_7_textbox;
 
-    Textbox* change_time_scale_instruction_textbox;
-
-    Textbox* change_mode_instruction_textbox;
-
-    Textbox* change_signal_instruction_textbox;
+    Textbox time_unit_textbox;
 
 
     // Объекты (кнопки настройки + кнопка смены сигнала)
-    
-    Button* decrease_value_scale_button;
-    Button* increase_value_scale_button;
+    Textbox change_value_scale_instruction_textbox;
+    Button decrease_value_scale_button;
+    Button increase_value_scale_button;
 
-    Button* decrease_time_scale_button;
-    Button* increase_time_scale_button;
+    Textbox change_time_scale_instruction_textbox;
+    Button decrease_time_scale_button;
+    Button increase_time_scale_button;
 
-    Button* change_mode_button;
+    Textbox change_freq_instruction_textbox;
+    Button decrease_freq_button;
+    Button increase_freq_button;
 
-    Button* change_signal_button;
+    Textbox change_mode_instruction_textbox;
+    Button change_mode_button;
+
+    Textbox change_signal_instruction_textbox;
+    Button change_signal_button;
 
 
 } scope_render_ctx;
+
+
+typedef struct scope_main_settings
+{
+
+    // Глобальные настройки
+
+    scope_mode_en current_mode;                     // Режим
+
+    int periods_to_display;                         // Количество периодов для отображения (в режиме с фикс. кол-вом)
+
+    // Текущие единицы измерения для отображения
+    signal_units current_signal_units;
+    time_units current_time_units;
+
+} scope_main_settings_ctx;
+
+
+typedef struct scope_signal_control_ctx
+{
+
+    sin_generator_ctx* controlled_signal;           // Контролируемый сигнал (в данной версии - только синус)
+
+    scope_buffer_ctx scope_buffer_data;             // Буфер осциллографа
+
+    // ==== Анализ сигнала для рескейла дисплея в моде с фикс. кол-вом периодов ====
+
+    // Данные о периоде
+    int current_period_value;
+    int current_frequency_value;
+
+    int zero_crossing_timestamps
+
+
+    int current_max_signal_value;
+    int current_min_signal_value;
+
+} scope_signal_control_ctx;
 
 
 // ===== Scope =====
 
 typedef struct scope {
 
-    scope_buffer_ctx scope_buffer_data;             // Буфер осциллографа
+    scope_main_settings_ctx main_settings;           // Основные настройки осциллографа (в данной версии - режим отображения и кол-во периодов для отображения в режиме с фикс. кол-вом)
 
-
-    sin_generator_ctx* controlled_signal;           // Контролируемый сигнал (в данной версии - только синус)
-
+    scope_signal_control_ctx signal_control_data;   // Данные контролируемого сигнала
 
     scope_render_ctx scope_render_data;             // Данные для рендеринга
 
@@ -152,17 +455,14 @@ typedef struct scope {
 
 // =========================================================================================== INNER FUNCTIONS
 
-void scope_init(scope_ctx* used_scope);
+void scope_init(scope_ctx* used_scope, SDL_Renderer* renderer);
 
-void connect_scope_sensor(scope_ctx* used_scope, sin_generator_ctx* controlled_signal);
+void signal_check(scope_ctx* used_scope, sin_generator_ctx* controlled_signal);
 
 void scope_update(scope_ctx* used_scope);
 
-void scope_render(scope_ctx* used_scope, SDL_Renderer renderer);
+void scope_render(scope_ctx* used_scope);
 
-void scope_gui_renew(scope_ctx* used_scope);
-
-void scope_data_clear(scope_ctx* used_scope);
 
 // Тут нет необходимости в сеттерах и геттерах, потому что структурное ООП - always public
 // я просто иницирую дефолтный осциллограф, выделяю дефолтный буфер при инициализации, а затем
@@ -170,5 +470,7 @@ void scope_data_clear(scope_ctx* used_scope);
 // тиков, на котором это время было снято и запихиваю оба значения в кольцевой буфер. На рендере я в
 // зависимости от настроек масштабов развертки отрисовываю внутри дисплея нужный кусок буфера
 
+
+void scope_destroy(scope_ctx* used_scope);
 
 // =========================================================================================== INNER FUNCTIONS
